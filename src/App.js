6e0c566b0dcce45ecd6c19f21a08eb5205818cd4
sthavir123 <200050080@iitb.ac.in>
2023-05-24 11:36:18 +0530

    Choose file option added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
-
+import React , {Component, ChangeEvent,useState } from 'react';
 function App() {
+  
+  const [file,setFile] = useState();
+  const [text, setText] = useState([]);
+   const onChangeHandle = (e)=>{ 
+    if(e.target.files){
+      setFile(e.target.files[0]);  
+    }
+   };
+
+  const showFile = async (e) => {
+    e.preventDefault();
+    const reader = new FileReader();
+    reader.onload = async (e) => {
+      var temp = e.target.result.split('\n');
+      //console.log(temp.split('\n'));
+      setText(temp);
+      console.log(text);
+    };
+    reader.readAsText(e.target.files[0]);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
+
+      <body>
+        <input type="file" onChange={(e) => showFile(e)}/>
+        <div className=' left window '>
+          
+        </div>
+      </body>
+
     </div>
   );
 }
