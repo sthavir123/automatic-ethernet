c0567c4abdc83b6dc8aba8cd18221b731aaf3bed
sthavir123 <200050080@iitb.ac.in>
2023-05-26 01:05:07 +0530

    test commit

--- src/select-table.js
+++ src/select-table.js
@@ -1,111 +1,5 @@
 import React from "react";
 import text from "./App.js";
-// const Users = [
-//     {
-//         id: 1,
-//         selected: false,
-//         name: "Leanne Graham",
-//         email: "Sincere@april.biz",
-//         phone: "1-770-736-8031 x56442",
-//         website: "hildegard.org",
-//       },
-//       {
-//         id: 1,
-//         selected: false,
-//         name: "Leanne Graham",
-//         email: "Sincere@april.biz",
-//         phone: "1-770-736-8031 x56442",
-//         website: "hildegard.org",
-//       },
-//       {
-//         id: 1,
-//         selected: false,
-//         name: "Leanne Graham",
-//         email: "Sincere@april.biz",
-//         phone: "1-770-736-8031 x56442",
-//         website: "hildegard.org",
-//       },
-//       {
-//         id: 1,
-//         selected: false,
-//         name: "Leanne Graham",
-//         email: "Sincere@april.biz",
-//         phone: "1-770-736-8031 x56442",
-//         website: "hildegard.org",
-//       },
-//       {
-//         id: 1,
-//         selected: false,
-//         name: "Leanne Graham",
-//         email: "Sincere@april.biz",
-//         phone: "1-770-736-8031 x56442",
-//         website: "hildegard.org",
-//       },
-//       {
-//         id: 1,
-//         selected: false,
-//         name: "Leanne Graham",
-//         email: "Sincere@april.biz",
-//         phone: "1-770-736-8031 x56442",
-//         website: "hildegard.org",
-//       },
-//       {
-//         id: 1,
-//         selected: false,
-//         name: "Leanne Graham",
-//         email: "Sincere@april.biz",
-//         phone: "1-770-736-8031 x56442",
-//         website: "hildegard.org",
-//       },
-//       {
-//         id: 1,
-//         selected: false,
-//         name: "Leanne Graham",
-//         email: "Sincere@april.biz",
-//         phone: "1-770-736-8031 x56442",
-//         website: "hildegard.org",
-//       },
-//   {
-//     id: 1,
-//     selected: false,
-//     name: "Leanne Graham",
-//     email: "Sincere@april.biz",
-//     phone: "1-770-736-8031 x56442",
-//     website: "hildegard.org",
-//   },
-//   {
-//     id: 2,
-//     selected: false,
-//     name: "Ervin Howell",
-//     email: "Shanna@melissa.tv",
-//     phone: "010-692-6593 x09125",
-//     website: "anastasia.net",
-//   },
-//   {
-//     id: 3,
-//     selected: false,
-//     name: "Clementine Bauch",
-//     email: "Nathan@yesenia.net",
-//     phone: "1-463-123-4447",
-//     website: "ramiro.info",
-//   },
-//   {
-//     id: 4,
-//     selected: true,
-//     name: "Patricia Lebsack",
-//     email: "Julianne.OConner@kory.org",
-//     phone: "493-170-9623 x156",
-//     website: "kale.biz",
-//   },
-//   {
-//     id: 5,
-//     selected: false,
-//     name: "Chelsey Dietrich",
-//     email: "Lucio_Hettinger@annie.ca",
-//     phone: "(254)954-1289",
-//     website: "demarco.info",
-//   },
-// ];
 
 class SelectTableComponent extends React.Component {
   constructor(props) {
@@ -226,5 +120,5 @@ class SelectTableComponent extends React.Component {
     );
   }
 }
-
+///vvjvpijv
 export default SelectTableComponent;
